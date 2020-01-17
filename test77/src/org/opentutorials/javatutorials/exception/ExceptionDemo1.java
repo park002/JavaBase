6ee10d22bc7e38dbfcb446a2d222fec7b08d3b56
pjh <ekem159@naver.com>
2020-01-17 23:01:44 +0900

    try{} catch(){} 예외 (12/12)

--- test77/src/org/opentutorials/javatutorials/exception/ExceptionDemo1.java
+++ test77/src/org/opentutorials/javatutorials/exception/ExceptionDemo1.java
@@ -1,21 +1,32 @@
 package org.opentutorials.javatutorials.exception;
 
-class A {
+class Z {
 	private int[] arr = new int[3];
-	 A() { //생성자 =클래스 이름과 메소드 이름이 같은 것 
+
+	Z() { // 생성자 = 클래스 이름과 메소드 이름이 같은 것
 		arr[0] = 0;
 		arr[1] = 10;
 		arr[2] = 20;
 	}
+
 	public void z(int first, int second) {
-		System.out.println(arr[first] / arr[second]);
+		try {
+			System.out.println(arr[first] / arr[second]);
+		} catch (ArithmeticException e) {
+			System.out.println(e);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
+
 public class ExceptionDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		A a = new A();
-		a.z(10, 1);
+		Z vv = new Z();
+		vv.z(10, 1);
 	}
 
 }
